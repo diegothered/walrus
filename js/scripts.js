ca833a24f2c588f39878df1162d13b121b86c0bc
Diego Rojas and Jaime Takikawa <diego@>
2019-05-20 19:38:10 -0700

    add JQuery functionality and css styles

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+  $(".clickable").click(function() {
+    $(".walrus-showing").slideToggle();
+    $(".walrus-hidden").slideToggle();
+  });
+});
+// $(document).ready(function() {
+//   $("h1").click(function() {
+//   alert("This is a header.");
+//   });
+//
+//   $("p").click(function() {
+//     alert("This is a paragraph.")
+//   });
+//
+//   $("img").click(function() {
+//     alert("This is an image.")
+//   });
+// })
